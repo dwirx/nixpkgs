1d9319b9dd11bc17a9426f830f0f7ea68cfa3559
dwirx <bangundwir@gmail.com>
2024-02-25 21:02:55 +0700

    update git

--- home/git.nix
+++ home/git.nix
@@ -16,6 +16,7 @@
       # Branches sorted by last modified
       # List Aliases
       al = "!git config -l | grep alias | cut -c 7-";
+      acp = "!f() { git add . && git commit -m \"$1\" && git push; }; f";
 
 
     };
