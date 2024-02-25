f4e93faabd069bd17e0279e21cc40b63b9fc3a10
dwirx <bangundwir@gmail.com>
2024-02-25 22:58:14 +0700

    updaate

--- home/home.nix
+++ home/home.nix
@@ -72,48 +72,6 @@
     enableFishIntegration = true;
   };
 
-  # zsh
-  programs.zsh = {
-    enable = true;
-    oh-my-zsh.enable = true;
-    syntaxHighlighting.enable = true;
-    enableAutosuggestions = true;
-    enableCompletion = true;
-    defaultKeymap = "viins";
-    oh-my-zsh.plugins = [
-      "git"
-      "sudo"
-      "vi-mode"
-      "z"
-    ];
-    shellAliases = {
-      # ls = "lsd";
-      cp = "cp -iv";
-      # mv = "mv iv";
-      rm = "trash-put";
-      cat = "bat";
-      vim = "nvim";
-      lg = "lazygit";
-      mans = '' man -k  . | cut -d " " -f 1 | fzf -m --preview "man {1}" '';
-      grep = "rg";
-      nf = "neofetch";
-    };
-    initExtraFirst = ''
-      # vi mode confi
-      VI_MODE_RESET_PROMPT_ON_MODE_CHANGE=true
-      VI_MODE_SET_CURSOR=true
-      MODE_INDICATOR="%F{yellow}+%f"
-      KEYTIMEOUT=15
-      VI_MODE_PROMPT_INFO=true
-
-      ### "bat" as manpager
-      # export MANPAGER="sh -c 'col -bx | bat -l man -p'"
-
-
-
-
-    '';
-  };
   # TMUX
   programs.tmux = {
     enable = true;
@@ -180,6 +138,7 @@
     ./shell.nix
     ./config.nix
     ./git.nix
+    ./zsh.nix
 
   ];
   programs = {
--- home/zsh.nix
+++ home/zsh.nix
@@ -1,40 +1,44 @@
 { ... }: {
-  programs.btop = {
+  # zsh
+  programs.zsh = {
     enable = true;
-    settings = {
-      color_theme = "dracula";
-      theme_background = false;
-      truecolor = true;
-      force_tty = false;
-      presets = "cpu:1:default,proc:0:default cpu:0:default,mem:0:default,net:0:default cpu:0:block,net:0:tty";
-
-      vim_keys = false;
-      rounded_corners = true;
-      graph_symbol = "braille";
-      shown_boxes = "mem net proc cpu";
-      update_ms = 250;
-      proc_sorting = "cpu lazy";
-      proc_reversed = false;
-      proc_tree = false;
-      proc_colors = true;
-      proc_gradient = false;
-      proc_per_core = true;
-      proc_mem_bytes = true;
-      proc_cpu_graphs = true;
-      proc_info_smaps = false;
-      proc_left = true;
-      proc_filter_kernel = false;
-      cpu_graph_upper = "total";
-      cpu_graph_lower = "user";
-      cpu_invert_lower = true;
-      cpu_single_graph = false;
-      cpu_bottom = false;
-      show_uptime = true;
-      check_temp = true;
-      cpu_sensor = "Auto";
-      show_coretemp = true;
-      cpu_core_map = "";
-      temp_scale = "celsius";
+    oh-my-zsh.enable = true;
+    syntaxHighlighting.enable = true;
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    defaultKeymap = "viins";
+    oh-my-zsh.plugins = [
+      "git"
+      "sudo"
+      "vi-mode"
+      "z"
+    ];
+    shellAliases = {
+      # ls = "lsd";
+      cp = "cp -iv";
+      # mv = "mv iv";
+      rm = "trash-put";
+      cat = "bat";
+      vim = "nvim";
+      lg = "lazygit";
+      mans = '' man -k  . | cut -d " " -f 1 | fzf -m --preview "man {1}" '';
+      grep = "rg";
+      nf = "neofetch";
     };
+    initExtraFirst = ''
+      # vi mode confi
+      VI_MODE_RESET_PROMPT_ON_MODE_CHANGE=true
+      VI_MODE_SET_CURSOR=true
+      MODE_INDICATOR="%F{yellow}+%f"
+      KEYTIMEOUT=15
+      VI_MODE_PROMPT_INFO=true
+
+      ### "bat" as manpager
+      # export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+
+
+
+
+    '';
   };
 }
