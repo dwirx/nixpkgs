c0f188b5a94b1ba5707a77bc1b2e63f31439c98e
dwirx <bangundwir@gmail.com>
2024-02-25 21:16:05 +0700

    uupdate

--- home/git.nix
+++ home/git.nix
@@ -12,11 +12,19 @@
       aa = "!git add -A && git status";
       cma = "!git add -A && git commit -m";
 
+      ac = "!git add . && git commit -m \"$1\" && :";
+      acp = "!git add . && git commit -m \"$1\" && git push";
+      acnc = "git add . && git commit --allow-empty-message -m \"\"";
 
-      # Branches sorted by last modified
+      # list commits in short form, with colors and branch/tag annotations
+      hist = "log --pretty=format:'%C(yellow)%h\\ %ad%Cred%d\\ %Creset%s%Cgreen\\ [%cn]' --decorate --date=short";
+      hsit = "!git hist";
+
+      # pass in the name or email address of someone involved with the project
+      whois = "!sh -c 'git log -i -1 --pretty=\"format:%an <%ae>\n\" --author=\"$1\"' -";
+
       # List Aliases
       al = "!git config -l | grep alias | cut -c 7-";
-      acp = "!f() { git add . && git commit -m \"$1\" && git push; }; f";
 
 
     };
