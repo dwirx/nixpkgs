7de3acd86c8367287162f952419edaee456a0415
dwirx <bangundwir@gmail.com>
2024-03-03 16:05:29 +0700

    update plugin nvim

--- config/nvim/lua/plugins/nvimtree.lua
+++ config/nvim/lua/plugins/nvimtree.lua
@@ -9,7 +9,7 @@ function M.config()
     ["<leader>e"] = { "<cmd>NvimTreeToggle<CR>", "Explorer" },
   }
 
-  local icons = require "hades.icons"
+  local icons = require "user.icons"
 
   require("nvim-tree").setup {
     hijack_netrw = false,
